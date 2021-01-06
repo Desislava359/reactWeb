79862880fbb31747006118e29aaf215d6c5b1022
Evgeniy Raev <evgeni.raev@dhl.com>
2021-01-06 21:54:51 +0200

    fixing navigation and rendering

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,43 +1,41 @@
 import React from 'react';
  
-import { BrowserRouter, NavLink } from 'react-router-dom';
+import {  Link } from 'react-router-dom';
 import './MainStyle.css'
  
 const Navigation = () => {
     return (
-        <BrowserRouter>
-
-         <div className="albums"> 
-            <div className="responsive">
-                <div className="galleries">
-                    <NavLink to="/fGallery" target="_blank"><img src="https://www.w3schools.com/css/img_5terre.jpg" alt="fBasic"/></NavLink>
-                    <p>First album</p>
-                </div>
-            </div>
-
-            <div className="responsive">
-                <div className="galleries">
-                    <NavLink to="/sGallery" target="_blank"><img src="https://www.w3schools.com/css/img_forest.jpg" alt="sBasic"/></NavLink>
-                    <p>Second Album</p>
-                </div>
-            </div>
-
-            <div className="responsive">
-                <div className="galleries">
-                    <NavLink to="/tGallery" target="_blank"><img src="https://www.w3schools.com/css/img_lights.jpg" alt="tBasic"/></NavLink>
-                    <p>Third Album</p>
-                </div>
-            </div>
-
-            <div className="responsive">
+        <div>
+            <div className="albums">
+                <div className="responsive">
                     <div className="galleries">
-                    <NavLink to="/favGallery" target="_blank"><img src="https://cdn.the-scientist.com/assets/articleNo/66864/aImg/35078/foresttb-m.jpg" alt="tBasic"/></NavLink>
-                    <p>Favorites</p>
+                        <Link to="/fGallery"><img src="https://www.w3schools.com/css/img_5terre.jpg" alt="fBasic" /></Link>
+                        <p>First album</p>
                     </div>
-            </div> 
-        </div>
+                </div>
 
-       </BrowserRouter>
+                <div className="responsive">
+                    <div className="galleries">
+                        <Link to="/sGallery"><img src="https://www.w3schools.com/css/img_forest.jpg" alt="sBasic" /></Link>
+                        <p>Second Album</p>
+                    </div>
+                </div>
+
+                <div className="responsive">
+                    <div className="galleries">
+                        <Link to="/tGallery"><img src="https://www.w3schools.com/css/img_lights.jpg" alt="tBasic" /></Link>
+                        <p>Third Album</p>
+                    </div>
+                </div>
+
+                <div className="responsive">
+                    <div className="galleries">
+                        <Link to="/favGallery"><img src="https://cdn.the-scientist.com/assets/articleNo/66864/aImg/35078/foresttb-m.jpg" alt="tBasic" /></Link>
+                        <p>Favorites</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
  
